9c71ec333ffa0f1e17632d625dedb02671c1844c
Daniel Faiole <danfaiole@gmail.com>
2022-10-03 13:25:51 -0300

    Added tests for services

---
+++ spec/services/instagram_service_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "InstagramService" do
+  let(:good_response) do
+    OpenStruct.new({
+      code: 200,
+      body: { test: '123' }.to_json
+    })
+  end
+  let(:bad_response) do
+    OpenStruct.new({
+      code: 500,
+      body: "I'm in a world of API"
+    })
+  end
+
+  describe ".update" do
+    context "when a request is avaliable" do
+      before do
+        allow(HTTParty).to receive(:get).and_return(good_response)
+      end
+
+      it "saves the request in the cache" do
+        expect { InstagramService.update }.to change { Instagram.count }.by(1)
+      end
+    end
+
+    context "when the endpoint does not respond" do
+      before do
+        allow(HTTParty).to receive(:get).and_return(bad_response)
+      end
+
+      it "saves the request in the cache" do
+        expect { InstagramService.update }.to_not change { Instagram.count }
+      end
+    end
+  end
+end
